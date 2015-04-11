ba0511f039140bc258dd0530dd75ff148163aea2
Justin Joseph <justin.joseph0@yahoo.com>
2015-04-11 19:03:10 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,19 @@
 class ProfilesController < ApplicationController
   def new
-    
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile
   end
   
   def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
     
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to user_path(params[:user_id])
+    else
+      redirect action: :new
+    end
   end
   
   def edit
@@ -13,5 +22,10 @@ class ProfilesController < ApplicationController
   
   def update
     
+  end
+  
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
   end
 end
